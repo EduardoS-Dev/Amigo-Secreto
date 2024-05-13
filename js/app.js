d1543c00d9ab0e3d6e4903f1c16a79737e9539e4
Eduardo Soares <eduardo.soaressilva@icloud.com>
2024-05-13 20:40:17 -0300

    Atualizando o código

--- js/app.js
+++ js/app.js
@@ -1,10 +1,43 @@
-let listaAmigos = []
+let amigos = [];
 
 function adicionar(){
-    let nomeAmigo = document.getElementById('nome-amigo').value;
-    let campoListaAmigos = document.getElementById('lista-amigos');
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
 
-    listaAmigos.push(' ' + nomeAmigo);
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
 
-    campoListaAmigos.innerHTML = `${listaAmigos}`;
+    amigos.push(amigo.value)
+
+    amigo.value = '';
+}
+
+function sortear(){
+    embaralha(amigos)
+    let sorteio = document.getElementById('lista-sorteio')
+
+    for(let i = 0; i < amigos.length; i++){
+
+        if(i == amigos.length - 1) {
+            sorteio.innerHTML += `${amigos[i]} --> ${amigos[0]}<br>` 
+        } else {
+            sorteio.innerHTML += `${amigos[i]} --> ${amigos[i + 1]}<br>` 
+        }
+    }
+    
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
